fc29c607ee86eabb42d055508dca3ccd64ffcb58
Koushic Kannan <koushic-kannan.raju@hpe.com>
2018-04-18 20:28:55 +0530

    Latest code update

--- src/driverScript/StringSampler.java
+++ src/driverScript/StringSampler.java
@@ -1,7 +1,7 @@
 package driverScript;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.HashMap;
 
 public class StringSampler {
@@ -10,10 +10,20 @@ public class StringSampler {
 
 		String mainArray = "My name is koushic Kannnan ";
 		String sumArray = "Passed=40 Failed=20 Skipped=15";
-		
+
 		/*longestString(mainArray);
-		repeatingString(mainArray);*/
+		repeatingString(mainArray);
 		sumString(sumArray);
+		isPalindrome("koushic");*/
+		
+		/*smallestElement();
+		largestElement();
+		swapNumbers(4,5);
+		*/
+		/*reverseNumbers(1560);*/
+		
+		int arr[] ={11,22,3,34,9,19,234}; 
+		bubbleSortAlgo(arr);
 
 	}
 
@@ -30,56 +40,179 @@ public class StringSampler {
 
 			}
 		}
-		
-		System.out.println("Longest word - "+rt);
-		System.out.println("Longest word length - "+rt.length());
+
+		System.out.println("Longest word - " + rt);
+		System.out.println("Longest word length - " + rt.length());
 
 	}
-	
-	public static void repeatingString(String repeatStr) throws IOException{
-		
+
+	public static void repeatingString(String repeatStr) throws IOException {
+
 		int len = repeatStr.length();
-		
+
 		HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
-		
-		for(int i=0;i<len;i++) {
-			
+
+		for (int i = 0; i < len; i++) {
+
 			char charAt = repeatStr.charAt(i);
-			
-			if(!hashMap.containsKey(charAt)) {
-				
+
+			if (!hashMap.containsKey(charAt)) {
+
 				hashMap.put(charAt, 1);
-			}else {
-				
-				hashMap.put(charAt, hashMap.get(charAt) +1);
+			} else {
+
+				hashMap.put(charAt, hashMap.get(charAt) + 1);
 			}
 		}
-		
+
 		System.out.println(hashMap);
-		
+
 	}
-	
-	
+
 	public static void sumString(String sumStr) throws IOException {
-		
-		ArrayList<String> al = new ArrayList<String>();
+
 		String[] space = sumStr.split(" ");
-		
-		
+
 		String[] pass = space[0].split("=");
 		String[] fail = space[1].split("=");
 		String[] skip = space[2].split("=");
-		
+
 		int passvalue = Integer.valueOf(pass[1]);
 		int failvalue = Integer.valueOf(fail[1]);
 		int skipvalue = Integer.valueOf(skip[1]);
+
+		System.out.println("Total Sum :: " + (passvalue + failvalue + skipvalue));
+
+	}
+	
+	public static void isPalindrome(String str) throws IOException{
 		
-		System.out.println("Total Sum :: "+( passvalue+failvalue+skipvalue));
+		int len = str.length();
+		String rev = "";
+		
+		for(int i= len - 1;i>=0;i--) {
+			
+			rev = rev + str.charAt(i);
+			
+		}
+			
+			if(str.equals(rev)){
+				
+				System.out.println(str + " is a Palindrome");
+				
+			}else {
+				System.out.println(str + " is not Palindrome");
+			}
+		}
+	
+	public static void smallestElement() throws IOException{
+		
+		int small;
+		int a[] = {12,45,9,34,112,22,5};
 		
 		
-	    
-	    
-	   
+		int size = a.length;
+		
+		/*System.out.println(size);*/
+		
+		small = a[0];
+		
+		for(int i=0;i<size;i++) {
+			
+			if(small > a[i]) {
+				
+				small = a[i];
+			}
+		}
+		
+		System.out.println("Smallest element :: "+ small);
+	}
+	
+	public static void largestElement() throws IOException{
+		
+		int small;
+		int a[] = {12,45,9,34,112,22,5};
+		
+		
+		int size = a.length;
+		
+		/*System.out.println(size);*/
+		
+		small = a[0];
+		
+		for(int i=0;i<size;i++) {
+			
+			if(small < a[i]) {
+				
+				small = a[i];
+			}
+		}
+		
+		System.out.println("Largest element :: "+ small);
 	}
 
+	public static void swapNumbers(int num1, int num2) throws IOException{
+		
+		int swap;
+		
+		System.out.println("Before swapping :: num1 ::"+num1 +" num2:: "+ num2);
+		
+		swap = num1;
+		num1 = num2;
+		num2 = swap;
+		
+		System.out.println("After swapping :: num1 ::"+num1 +" num2:: "+ num2);
+		
+		
+	}
+	
+	public static void reverseNumbers(int num) throws IOException{
+		
+		int r, re =0;
+		
+		while(num !=0) {
+			
+			r = num%10;
+			re = re*10 +r ;
+			num = num/10;
+		}
+		
+		System.out.println("Reverse :: "+re);
+	}
+
+	
+	public static void bubbleSortAlgo(int arr[]) throws IOException{
+		
+		int n = arr.length;
+		
+		int temp =0;
+		
+		for(int i=0; i<n -1; i++) {
+			
+			for(int j=0; j<(n-i-1);j++) {
+				
+				if(arr[j] > arr[j+1]) {
+					
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					
+					
+				}
+				
+				
+			}
+			
+			
+			
+		}
+		
+		for(int m=0;m<n;m++) {
+			
+			System.out.print(arr[m]+" ");
+			System.out.println();
+		}
+		
+		
+	}
 }
--- src/driverScript/WindowsHandler.java
+++ src/driverScript/WindowsHandler.java
@@ -1,11 +1,16 @@
 package driverScript;
 
+import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.support.ui.FluentWait;
@@ -19,15 +24,31 @@ public class WindowsHandler {
 			.withTimeout(60, TimeUnit.SECONDS)
 			.pollingEvery(5, TimeUnit.SECONDS)
 			.ignoring(NoSuchElementException.class);
-
-	public static void main(String[] args) throws InterruptedIOException {
-
+	
+	
+	public static void main(String[] args) throws IOException, ParseException{
+		
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\rajuk\\workspace\\python\\Selenium\\drivers\\chromedriver.exe");
 
 		 driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		
+		/*windowsHandler();
+		dynamicValuesHandler();
+		dynamicValuesMaxPrice();*/
+		
+		/*getAllValuesfromTable();*/
+		
+		isFrameSamp();
+		
+	}
+
+	
+	public static void windowsHandler() throws InterruptedIOException {
+
+		
 
 		driver.get("http://demo.guru99.com/popup.php");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -60,5 +81,129 @@ public class WindowsHandler {
 		driver.switchTo().window(mainWindow);
 		driver.close();
 	}
+	
+	/*Fetch cell value of a particular row and column of the Dynamic Table*/
+	public static void dynamicValuesHandler() throws IOException{
+		
+		driver.get("https://money.rediff.com/gainers/bsc/daily/groupa?");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement baseTable = driver.findElement(By.tagName("table"));
+		
+		WebElement rowCount = baseTable.findElement(By.xpath("//*[@id='leftcontainer']/table/tbody/tr[3]"));
+		
+		WebElement rowCountText = baseTable.findElement(By.xpath("//*[@id='leftcontainer']/table/tbody/tr[3]/td[1]"));
+		
+		String rowText = rowCountText.getText();
+		
+		System.out.println("Row text :: "+ rowText);
+		
+		WebElement columnCount = rowCount.findElement(By.xpath("//*[@id='leftcontainer']/table/tbody/tr[3]/td[2]"));
+		
+		String columnText = columnCount.getText();
+		System.out.println("Cell Value is :: "+columnText);
+		
+		driver.close();
+	}
 
+	/*Get Maximum of all the Values in a Column of Dynamic Table*/
+	public static void dynamicValuesMaxPrice() throws IOException, ParseException {
+		
+		String max;
+		double m =0, n =0;
+
+		driver.get("https://money.rediff.com/gainers/bsc/daily/groupa?");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		List<WebElement> row = driver.findElements(By.xpath(".//*[@id='leftcontainer']/table/tbody/tr/td[1]"));
+		
+		System.out.println("Row count size :: "+ row.size());
+		
+		List<WebElement> col = driver.findElements(By.xpath(".//*[@id='leftcontainer']/table/thead/tr/th"));
+
+		System.out.println("Column count size ::" + col.size());
+		
+		for(int i = 1; i<row.size();i++) {
+			
+			max = driver.findElement(By.xpath("html/body/div[1]/div[5]/table/tbody/tr[" + (i+1)+ "]/td[4]")).getText();
+			
+			NumberFormat nf = NumberFormat.getNumberInstance();
+			
+			Number num = nf.parse(max);
+			
+			max = num.toString();
+			
+			m = Double.parseDouble(max);
+			
+			if(m > n) {
+				
+				n = m;
+			}
+		}
+		
+		System.out.println("Maximum current price is : "+ n);
+
+		
+		driver.close();
+	}
+	
+	
+	/*Get all the values of a Dynamic Table*/
+	public static void getAllValuesfromTable() throws IOException{
+		
+		driver.get("http://demo.guru99.com/test/table.html");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement table = driver.findElement(By.xpath("/html/body/table/tbody"));
+		
+		List<WebElement> row = table.findElements(By.tagName("tr"));
+		
+		int rowCount = row.size();
+		
+		for(int r=0; r<rowCount; r++) {
+			
+			List<WebElement> column = row.get(r).findElements(By.tagName("td"));
+			
+			int colCount = column.size();
+			
+			
+			for(int i=0; i <colCount; i++ ) {
+				
+				String getText = column.get(i).getText();
+				
+				System.out.println("cell value of row number "+r+ " and column number "+i+ " is "+getText);
+				
+				
+			}
+			
+			System.out.println("--------------------------------------------------");
+			
+			
+		}
+		
+		driver.close();
+		
+	
+		
+	}
+
+	
+	public static void isFrameSamp() throws IOException{
+		
+		
+		driver.get("https://www.softwaretestingmaterial.com/generate-extent-reports/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		int iFramesize = driver.findElements(By.tagName("iframe")).size();
+		
+		
+		System.out.println("iFrames Count :: "+iFramesize);
+		
+		driver.switchTo().frame("aswift_0");
+		
+		System.out.println("********We are switched to the iframe*******");
+		
+		driver.close();
+	}
 }
